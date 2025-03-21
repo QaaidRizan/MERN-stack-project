a35490ee27d35e415c0f0a467f52e61d52318ede
M R M Qaaid <qaaidrizan113@gmail.com>
2025-03-21 12:15:03 -0700

    last

--- AdminPanale/src/App.jsx
+++ AdminPanale/src/App.jsx
@@ -19,10 +19,9 @@ const App = () => {
       <div className="app-content">
         <Sidebar />
         <Routes>
-          <Route path="/add" element={<Add/>} />
+          <Route path="/" element={<Add/>} />
           <Route path="/list" element={<List/>} />
           <Route path="/order" element={<Order/>} />
-        
         </Routes>
       </div>
     </div>
