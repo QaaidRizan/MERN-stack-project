23de81eb1632e95cea27d8dcb85dddf1406bb164
M R M Qaaid <qaaidrizan113@gmail.com>
2025-04-29 21:41:37 +0530

    last

--- frontend/src/components/Item/Item.jsx
+++ frontend/src/components/Item/Item.jsx
@@ -34,35 +34,7 @@ const CarItem = ({ id, name, description, price, images, year, mileage, transmis
         <h3 className="car-item-name">{name}</h3>
         <p className="car-item-description">{truncatedDescription}</p>
         
-        <div className="car-specs">
-          {mileage && (
-            <div className="car-spec-item">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
-              </svg>
-              <span>{formattedMileage} miles</span>
-            </div>
-          )}
-          
-          {transmission && (
-            <div className="car-spec-item">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                <rect x="2" y="6" width="20" height="12" rx="2"></rect>
-                <path d="M12 12h.01"></path>
-              </svg>
-              <span>{transmission}</span>
-            </div>
-          )}
-          
-          {fuelType && (
-            <div className="car-spec-item">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                <path d="M19 17h2c.6 0 1-.4 1-1v-3c0-.9-.7-1.7-1.5-1.9C18.7 10.6 16 10 16 10s-1.3-1.4-2.2-2.3c-.5-.4-1.1-.7-1.8-.7H5c-.6 0-1.1.4-1.4.9l-1.5 2.8C1.5 11.3 1 12.1 1 13v3c0 .6.4 1 1 1h2"></path>
-              </svg>
-              <span>{fuelType}</span>
-            </div>
-          )}
-        </div>
+      
         
         <Link to={`/place-order/${id}`} className="view-details-button">
           View Details
